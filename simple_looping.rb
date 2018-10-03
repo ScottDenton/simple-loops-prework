b6069f7616606155d08ff91816bfcef7d0dd1cad
scottyd_14 <scottyd_14@hotmail.com>
2018-10-03 17:57:49 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,10 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  number = number_of_times
-  counter = 1
-   phrase = "Welcome to Flatiron School's Web Development Course!"
+
   loop do
+    number = number_of_times
+    counter = 1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     counter +=1
     break if counter = number
