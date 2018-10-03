e55c10566f7f9c49a91c59e4c0494b000f9253f9
scottyd_14 <scottyd_14@hotmail.com>
2018-10-03 17:53:34 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,9 +1,14 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  counter = 0
   loop do
     phrase = "Welcome to Flatiron School's Web Development Course!"
-end
+    counter +=1
+    puts phrase
+    
+    break if counter = number_of_times
+  end
   
 end
 
